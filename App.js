7bf7b3b22eb47337c8aef4d9864cf75f5c751fb6
kaisarsofi-kxar <kaisersofi.ks2515@gmail.com>
2023-11-07 13:38:06 +0530

    final update for both ios and Andriod

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import AppNavigation from "./src/navigation";
+import AppNavigation from './src/navigation';
 
 export default function App() {
-  return <AppNavigation />;
+  return (
+    <AppNavigation />
+  );
 }
